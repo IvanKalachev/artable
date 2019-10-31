0a6cc14642ac64d4d605f8189ef321ac5131bf51
Ivan Kalachev <ivankalachev@Ivans-MacBook-Pro.local>
2019-10-31 15:17:17 +0200

    Initial commit

---
+++ Artable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Artable
+//
+//  Created by Ivan Kalachev on 31.10.19.
+//  Copyright © 2019 Ivan Kalachev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
